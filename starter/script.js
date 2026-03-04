83b6637231ea2e5286da5da6d0b2cac33d9bfec3
AnandaJoshi <anandjoshi050@gmail.com>
2026-03-04 22:33:51 +0530

    Fixed close account section and then hide UI

--- starter/script.js
+++ starter/script.js
@@ -201,3 +201,23 @@ btnTransfer.addEventListener('click', function(e) {
   }
   
 });
+
+//Close Account
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if(inputCloseUsername.value == currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ){
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+    // To check console.log(index);
+
+    //Delete Account
+    accounts.splice(index,1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+ 
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
