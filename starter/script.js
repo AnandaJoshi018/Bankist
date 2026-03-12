6b346d365db1d34f4f5c12549860356f414cfa22
AnandaJoshi <anandjoshi050@gmail.com>
2026-03-12 06:55:23 +0530

    Rounding the values and changed two accounts data

--- starter/script.js
+++ starter/script.js
@@ -5,11 +5,28 @@
 // BANKIST APP
 
 // Data
+// Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,6 +34,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -90,7 +120,7 @@ const displayMovements = function(movements,sort = false){
     <div class="movements__row">
         <div class="movements__type movements__type--${type}">
           ${i + 1} ${type} </div> 
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov.toFixed(2)}</div>
     </div>
   `;
 
@@ -108,7 +138,7 @@ const calcDisplayBalence = function(acc) {
   //movements = [200, -100] 
   //Iteration 1  :acc = 0(initial),mov = 200 ➡️ acc + mov = 200
   //teration 2 :acc = 200 , mov = -100 ➡️   acc + mov = 100
-  labelBalance.textContent = `${acc.balence} RUPEES`;
+  labelBalance.textContent = `${acc.balence.toFixed(2)} RUPEES`;
 }
 // To check Balence calcDisplayBalence(account1.movements);
 
@@ -116,12 +146,12 @@ const calcDisplaySummary = function(acc){
   const incomes = acc.movements
    .filter(mov => mov > 0) //filter() returns a new array.#Example💠[200, -100, 500, -50] ➡️ [200, 500]
    .reduce((acc,mov) => acc + mov , 0); //Give a single value for filterd array
-  labelSumIn.textContent = `${incomes}₹`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}₹`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc,mov) => acc + mov , 0);
-  labelSumOut.textContent = `${Math.abs(out)}₹`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}₹`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -131,7 +161,7 @@ const calcDisplaySummary = function(acc){
       return int >= 1;
     })
     .reduce((acc,int) => acc + int , 0); ////Give a single value for filterd array.
-  labelSumInterest.textContent = `${interest}₹`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}₹`;
 };
 //To check Summary -> calcDisplaySummary(account1.movements);
 
@@ -212,7 +242,7 @@ btnTransfer.addEventListener('click', function(e) {
 //Loan Amount Section
 btnLoan.addEventListener('click',function(e){
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value); //.Floor returns greatest integer or equal to its numeric argument
 
   if(amount > 0 && currentAccount.movements.some(mov => 
   mov >= amount * 0.1)){
